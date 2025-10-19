b1fcf8620e510438f6193a62264c48f998503f6d
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2025-10-19 14:44:04 +0530

    day 1 trining

--- practice1.py
+++ practice1.py
@@ -2,6 +2,7 @@
 fruit = 'banana'
 index = 0
 while index < len(fruit):# 0< lenth of fruit
-    letter = fruit[index]# store the fruit[] index in letter variable
+    letter = fruit[index]# 5 a store the fruit[] index in letter variable
     print(index,letter)#print index and letter
-    index = index + 1 # add 1 to evrey ilterrating value of index
+    index = index + 1 # 5 add 1 to evrey ilterrating value of index
+# after thr iltration there is nomore fruit index then inner itration is over
