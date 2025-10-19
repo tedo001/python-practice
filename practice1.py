e3d6f70c7461938549bde2fdcd18e1c828025c78
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2025-10-19 14:35:58 +0530

    day 1 trining

--- practice1.py
+++ practice1.py
@@ -1,7 +1,7 @@
 
 fruit = 'banana'
 index = 0
-while index < len(fruit):
-    letter = fruit[index]
-    print(index,letter)
-    index = index + 1
+while index < len(fruit):# 0< lenth of fruit
+    letter = fruit[index]# store the fruit[] index in letter variable
+    print(index,letter)#print index and letter
+    index = index + 1 # add 1 to evrey ilterrating value of index
