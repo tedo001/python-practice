564dd100a93a313d271d00a52cdbd4ebf19a05b2
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2025-10-19 14:54:58 +0530

    day 1 trining

--- looping and counting.py
+++ looping and counting.py
@@ -1,6 +1,6 @@
 word = 'banana'
 count = 0
-for letter in word:
-    if letter == 'a':
-      count = count + 1
-print(count)
+for letter in word:#
+    if letter == 'a':# 3A
+      count = count + 1#3A
+print(count)#3A
