8db9d5b91e9a126f4f88d29b8ef18c00b4fe2f82
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2025-12-14 16:04:45 +0530

    madmax

--- infineteloop.py
+++ infineteloop.py
@@ -1,2 +1,11 @@
-name = int(input('enter your name:'))
-print("cantidate name:",name)
+name = str(input('enter your name:'))
+id= int(input("enter your id:"))
+tokenno= int(input("enter a token.no:"))
+if tokenno==(1,2,3,4,5,6,7,8,9,10):
+       things=list(input("enter a things name to insert to the store:"))
+       thingname=str(things)
+       print("cantidate name:",name)
+       print("cantidate id:",id)
+       print("adden things are",thingname)
+else:
+    print("invalid entry token fail")
