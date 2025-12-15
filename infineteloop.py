12702c2dc28f2537ae8b926f088fd334f7b62e0b
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2025-12-15 10:15:47 +0530

    madmax

--- infineteloop.py
+++ infineteloop.py
@@ -1,14 +1,27 @@
 name = str(input('enter your name:'))
 id= int(input("enter your id:"))
-if id < 1000:
-    tokenno = int(input("enter a token.no:"))
-  if tokenno==(1,2,3,4,5,6,7,8,9,10):
-       things=list(input("enter a things name to insert to the store:"))
-       thingname=str(things)
-       print("cantidate name:",name)
-       print("cantidate id:",id)
-       print("adden things are",thingname)
-  else:
-       print("invalid entry token fail")
+id=id
+tokenno = list(input("enter a token.no:"))
+if (id > 1000):
+    print("invalid id")
+
+if(id<1000 and tokenno == 1,2,3,4,5,6,7,8,9,10):
+        things = list(input("enter a things name to insert to the store:"))
+        thingname = str(things)
+        print("cantidate name:", name)
+        print("cantidate id:", id)
+        print("adden things are", thingname)
+       # if tokenno == 1, 2, 3, 4, 5, 6, 7, 8, 9, 10:
+#else:
+        #print("invalid entry token fail")
 else:
-    print("invalid entry token fail")
+    print("wrong entry"
+          ""
+          ""
+          ""
+          ""
+          ""
+          ""
+          ""
+          ""
+          "")
