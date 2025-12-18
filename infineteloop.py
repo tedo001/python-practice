cc7b3618f8615f19570b5199adc8729a841a72f3
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2025-12-18 13:52:23 +0530

    day 5 python trining

--- infineteloop.py
+++ infineteloop.py
@@ -1,6 +1,9 @@
 import numpy as np
 
-arr=np.array([[[['a''b''c'],
-              ['d''e''f'],['g''h''i']]]])
-print(arr[0][0][1])
+arr=np.array([[[['a''b''c'],['d''e''f'],['g''h''i'],
+                ['j''k''l'],['m''n''o'],['p''q''r'],
+                ['s''t''u'],['v''w''x'],['y''z'' ']
+                ]]])
+
+print(arr[0][0][0])
 
