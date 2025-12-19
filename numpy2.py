1ab48efbffea79a34f70cf6a9b0f6e83c4d0ac93
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2025-12-19 21:31:25 +0530

    day 6 python trining numpy trining

--- numpy2.py
+++ numpy2.py
@@ -1,6 +1,1 @@
 import numpy as np
-arr = np.array([[['a','b','c'],['d','e','f'],['g','h','i']],
-                [['j','k','l'],['m','n','o'],['p','q','r']],
-                [['s','t','u'],['v','w','x'], ['y','z',' ']]])
-word= arr[0,2,2]+arr[2,2,2]+arr[2,2,2]+arr[0,2,0]+arr[1,2,2]+arr[0,1,1]+arr[0,0,0]#iam greate
-print(word)
