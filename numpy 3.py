1d59c75b6186e22904ee7d946adf88cf4108b3a7
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2025-12-19 21:41:44 +0530

    day 6 python trining numpy trining

---
+++ numpy 3.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr=np.array([[['a','b','c'],['d','e','f'],['g','h','i']],
+             [['j','k','l'],['m',',n','o'],['p','q','r']],
+             [['s','t','u'],['v','w','x'],['y','z',' ']]])
+word =array(input())
+for arr in range(arr):
+word1 = word+word
+print(word1)
