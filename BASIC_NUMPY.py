6725a3fbc0372cfbbae4066d93f08216c458dc24
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2025-12-21 20:35:20 +0530

    day 7 python trining numpy trining

--- BASIC_NUMPY.py
+++ BASIC_NUMPY.py
@@ -1,10 +1,12 @@
 import numpy as np
 
+
 arr=np.array([[['a','b','c'],['d','e','f'],['g','h','i']],
              [['j','k','l'],['m',',n','o'],['p','q','r']],
              [['s','t','u'],['v','w','x'],['y','z',' ']]])
 
+
 word = arr[0,0,0]+arr[1,2,0]+arr[1,2,0]+arr[1,0,2]+arr[0,1,1]
 
 print(word)
-print(arr.shape)#3lAYER,3ROW,3COLOUM
+print(arr.shape)##3lAYER,3ROW,3COLOUM
--- numpy2.py
+++ numpy2.py
@@ -1,1 +1,22 @@
 import numpy as np
+
+arr = np.array([
+    [['a','b','c'], ['d','e','f'], ['g','h','i']],
+    [['j','k','l'], ['m','n','o'], ['p','q','r']],
+    [['s','t','u'], ['v','w','x'], ['y','z',' ']]
+])
+
+word = ""
+
+n = int(input("How many letters do you want? "))
+
+for i in range(n):
+    print(f"Enter indexes for letter {i+1}")
+    k = int(input("Layer : "))
+    r = int(input("Row : "))
+    c = int(input("Column : "))
+
+    word += arr[k][r][c]
+
+print("Word formed:", word)
+print("Array shape:", arr.shape)
