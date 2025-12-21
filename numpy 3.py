67976182261f59976efecdee247076c5e9e28ea0
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2025-12-21 14:26:40 +0530

    day 7 python trining numpy trining

--- numpy 3.py
+++ numpy 3.py
@@ -1,9 +1,6 @@
-import numpy as np
-
-arr=np.array([[['a','b','c'],['d','e','f'],['g','h','i']],
-             [['j','k','l'],['m',',n','o'],['p','q','r']],
-             [['s','t','u'],['v','w','x'],['y','z',' ']]])
-word =array(input())
-for arr in range(arr):
-word1 = word+word
-print(word1)
+a=[]
+print("enter 10 numbers:")
+for i in range(5):
+    num=int(input("enter num:"))#+str(i+1)
+    a.append(num)
+print(a)
