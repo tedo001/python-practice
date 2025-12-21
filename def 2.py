cbc49057a18b30fda9b9e9060a9254a2c5083fd0
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2025-12-22 03:18:12 +0530

    day 7 python trining numpy trining

---
+++ def 2.py
@@ -0,0 +1,11 @@
+#################################
+def add(x,y=50):
+    print('x:',x)
+    print('y:',y)
+add(10)
+print(add(12))
+#################################
+def todo(eating,studing):
+    print(eating,studing)
+todo(eating=10,studing=20)
+todo(studing=20,eating=10)
