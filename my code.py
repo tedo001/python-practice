4d1e2fc74923bb02bf78fb3fef1e88e2ce775695
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2025-12-27 21:39:48 +0530

    day 13 python trining numpy trining

--- my code.py
+++ my code.py
@@ -1,9 +1,4 @@
 
-# Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
 
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
