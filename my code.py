d982fba3d672bfa3326a98c0c1e16711b2fc0194
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2025-12-27 21:39:20 +0530

    day 13 python trining numpy trining

--- my code.py
+++ my code.py
@@ -1,14 +1,29 @@
-set=["blue","blue","blue","blue","red","blue","blue","blue","red","",""]
-balls=[]
-get=[]
-for i in range(len(set)):
-    balls=set[i-1]
 
-    if "red" in balls:
-        print("red ball indice is",i-1)
-        get= balls
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-    else:
-        print("indice",i-1,"blue ball")
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        dummy = ListNode(0)
+        current = dummy
+        carry = 0
 
-print(balls)
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+
+            total = v1 + v2 + carry
+            carry = total // 10
+
+            current.next = ListNode(total % 10)
+            current = current.next
+
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+
+        return dummy.next
