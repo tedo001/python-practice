223be9d644299a6b34c827e4337c3b9b96b893c5
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2025-12-28 20:56:17 +0530

    day 14

--- dsa/linkedlist.py
+++ dsa/linkedlist.py
@@ -1,17 +1,4 @@
-class Numbers:
-    def add(self, num1, num2):
-        return num1 + num2
-
-    def sub(self, num1, num2):
-        return num1 - num2
-    def mul(self, num1, num2):
-        return num1 * num2
-    def div(self, num1, num2):
-        return num1 / num2
-    def pow(self, num1, num2):
-        return num1 ** num2
-
-
+from numbers import numbers:
 class LinkedList:
     def __init__(self):
         self.calc = Numbers()
