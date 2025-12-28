c59dfef8ddbcdd637648669fc80c7d9f2b954229
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2025-12-28 20:26:44 +0530

    day 14

--- linked list.py
+++
@@ -1,8 +0,0 @@
-class backer(Node):
-    def __init__(self, data,next=None):
-        self.data = data
-        self.next = next
-class core(next):
-       def __init__(self, data,next=None):
-           self.data = data
-           self.next = next
