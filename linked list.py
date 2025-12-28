a4cd0312d923798dedc44b375fa9f0c8db665d13
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2025-12-28 20:35:18 +0530

    day 14

--- linked list.py
+++ linked list.py
@@ -1,3 +1,7 @@
 class numbers(object):
-    def __init__(self):
-
+       def add(num1,num2):
+           fun=num1+num2
+           return fun
+       def sub(num1-num2):
+            fun2=num1-num2
+            return fun2
