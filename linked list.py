5896a00d95781c4f289f57a42dd396ac6839b4c8
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2025-12-28 20:45:11 +0530

    day 14

--- linked list.py
+++ linked list.py
@@ -6,4 +6,13 @@ class Numbers(object):
         return num1 - num2
 
 
-#
+# create object
+obj = Numbers()
+
+# input
+b = int(input("Enter first number: "))
+c = int(input("Enter second number: "))
+
+# method calls
+print("Addition:", obj.add(b, c))
+print("Subtraction:", obj.sub(b, c))
