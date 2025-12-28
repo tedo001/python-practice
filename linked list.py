5c069a82641d0284fd001c79f694a66f2b441d84
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2025-12-28 20:41:10 +0530

    day 14

--- linked list.py
+++ linked list.py
@@ -1,7 +1,14 @@
 class numbers(object):
-       def add(num1,num2):
+       def add(self,num1,num2):
            fun=num1+num2
            return fun
-       def sub(num1-num2):
-            fun2=num1-num2
-            return fun2
+       def sub(self,num1,num2):
+           fun2=num1-num2
+           return fun2
+class linked_list(object):
+    age=numbers()
+    b=int(input())
+    c=int(input())
+    n1=numbers.b
+    n2=numbers.c
+print(numbers(self,n1,n2))
