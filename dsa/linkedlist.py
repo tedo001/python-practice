f7a35956befdd70683435582f7260c80132ce31a
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2025-12-28 20:54:00 +0530

    day 14

--- dsa/linkedlist.py
+++ dsa/linkedlist.py
@@ -4,6 +4,12 @@ class Numbers:
 
     def sub(self, num1, num2):
         return num1 - num2
+    def mul(self, num1, num2):
+        return num1 * num2
+    def div(self, num1, num2):
+        return num1 / num2
+    def pow(self, num1, num2):
+        return num1 ** num2
 
 
 class LinkedList:
