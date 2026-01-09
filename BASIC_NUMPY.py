e966dc689793f5218043917449114b0c630d3e1e
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-01-09 20:52:28 +0530

    day 15

--- BASIC_NUMPY.py
+++ BASIC_NUMPY.py
@@ -3,10 +3,10 @@ import numpy as np
 
 arr=np.array([[['a','b','c'],['d','e','f'],['g','h','i']],
              [['j','k','l'],['m',',n','o'],['p','q','r']],
-             [['s','t','u'],['v','w','x'],['y','z',' ']]])
+             [['s','t','u'],['v','w','x'],['y','z','']]])
 
 
 word = arr[0,0,0]+arr[1,2,0]+arr[1,2,0]+arr[1,0,2]+arr[0,1,1]
 
 print(word)
-print(arr.shape)##3lAYER,3ROW,3COLOUM
+print(arr.shape)
