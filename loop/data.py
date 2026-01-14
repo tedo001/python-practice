87df8d53eaafaceaef05574562133448c1b601da
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-01-14 15:52:59 +0530

    m

--- loop/data.py
+++ loop/data.py
@@ -1,10 +1,8 @@
 class data:
      def data_1(self,fand):
 
-         fand={"man","han","gago","fish","cons","fandom","manisk","cows","cow","fish",fand.lower(),fand.upper()}
-
-
+         fand={"man","han","gago","fish","cons","fandom","manisk","cows","cow","fish",fand.lower(),fand.upper(),fand.strip()}
          return fand
 d1=data()
-
-print(d1.data_1("man"))
+f="sam"
+print(d1.data_1(f))
