bd9782db7b1fc236e9431ef39e5f6d04a21aa0ba
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-01-14 16:03:14 +0530

    m

--- loop/ill.py
+++ loop/ill.py
@@ -1,9 +1,13 @@
-from data.py import data
-class obj:
+from data import data
+class linkedlist:
     def __init__(self):
         self.mat=data()
+    def calc(self,num):
+        self.mat=data()
+        for i in range(num):
+            return i
 
 
 
-num1=obj()
+num1=linkedlist()
 
