4bbadf51289e0fa5f3a26b80fcc2a03754b2bcd6
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-01-15 10:29:29 +0530

    day 31

---
+++ string.py/onebyone.py
@@ -0,0 +1,19 @@
+
+class app:
+    def __init__(self):
+        self.predict=self
+    def function(self,name,rollno,num2):
+     def display(self,rollno):
+        return self.predict.rollno
+     def display2(self,name):
+        return self.predict.name
+     def display3(self,num2):
+        return self.predict.num2
+
+a=str(input("enter a  name of a student :"))
+b=int(input("enter a rollno of a student :"))
+c=int(input("enter a number of a student:"))
+a1=app()
+a1.display()
+a1.display2()
+a1.display3()
