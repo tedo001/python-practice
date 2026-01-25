d27ffb533f83476c4a52b2ad611d765d0e652a06
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-01-26 02:18:18 +0530

    day 36

--- pattern/1.py
+++ pattern/1.py
@@ -1,3 +1,20 @@
-pivot=list(input("enter a shorted num:"))
+num=list(input("enter a shorted num:"))
 
-print(pivot[0])
+pivot=num[0]
+
+shorted=[]
+for i in range(len(num)):
+
+     if num[i]>pivot:
+        shorted.append(num[i])
+        print(shorted)
+     else:
+        print()
+
+        """for n in range(len(num2)):
+    if num2[n]>pivot2:
+        pivot2=num2[n]
+    else:
+        num2=pivot2.append(num2[n])"""
+
+
