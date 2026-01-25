3878bd45357e5add5c9c9a7e5f5f439a74d8a993
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-01-25 21:56:18 +0530

    day 35

--- pattern/1.py
+++ pattern/1.py
@@ -3,3 +3,8 @@ for i in range(10):
     for j in range(4,i+1):
         j="*"
         print("",j," ",end="")
+for i in range(10):
+    print()
+    for j in range(4,i+1):
+        j="*"
+        print("",j," ",end="")
