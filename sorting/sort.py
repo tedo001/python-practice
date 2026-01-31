90308c8fbf1a5a3e83aaf8a97b2ffc044f90641b
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-01-31 20:09:57 +0530

    day 36

--- sorting/sort.py
+++ sorting/sort.py
@@ -2,13 +2,13 @@ def sort(num):
      pivot=num[0]
      left=[]
      right=[]
-     for i in range(len(num)):
+     for i in range(len(num[1:])):
          if num[i]>pivot:
              left.append(num[i])
          elif num[i]<pivot:
              right.append(num[i])
          else:
              return num
-     return left + pivot + right
+     return sort.left + sort.pivot + sort.right
 num="876392"
 sort(num)
