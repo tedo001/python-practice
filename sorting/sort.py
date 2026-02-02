8949cd74b32b22dc8dd136b4c672406f728c6657
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-02-02 17:38:56 +0530

    day 47

--- sorting/sort.py
+++ sorting/sort.py
@@ -1,4 +1,22 @@
 def dd(num):
+    left=[]
+    right=[]
+    pivot=num[0]
+    for j in num:
+        if j>pivot:
+            left.append(j)
+        else:
+            right.append(j)
+    """
     sd=0
     for i in num:
         sd=sd+i
+    if sd==pivot:
+        return left
+    else:
+        return right
+     """
+    return [left+pivot+right]
+print(dd([1,2,3]))
+
+
