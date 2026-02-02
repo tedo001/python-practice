ffa79426c50d1435ab60c0f938c42e2a65c4f025
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-02-02 17:28:04 +0530

    day 36

--- sorting/sort.py
+++ sorting/sort.py
@@ -1,14 +1,4 @@
-def sort(nums1,nums2):
-    num3 = nums1+nums2
-    num3.sort()
-    count = 0
-    try:
-
-        for i in num3:
-            count = count + i
-
-    return count / len(num3)
-num1 =list(input())
-num2 =list(input())
-
-sort(num1,num2)
+def dd(num):
+    sd=0
+    for i in num:
+        sd=sd+i
