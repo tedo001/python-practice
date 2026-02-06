296b177cd8b6c9d78944679610755fe6161e2f6a
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-02-06 23:02:51 +0530

    day 48

--- codeforce/A. Blackslex and Password.py
+++ codeforce/A. Blackslex and Password.py
@@ -2,10 +2,19 @@ size=int(input())
 m=[]
 n=[]
 j=[]
-for i in range(1):
-    m.append(int(input()))
-    n.append(int(input()))
-    j.append(int(input()))
+for i in range(size):
+    if i==0:
+        m.append(int(input()))
+        continue
+    elif i==1:
+        n.append(int(input()))
+    else:
+        j.append(int(input()))
+
+
+
+n.append(int(input()))
+j.append(int(input()))
 m2=m[0]*m[1]
 n2=n[0]*n[1]
 j2=j[0]*j[1]
