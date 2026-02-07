06ed99a6432cf2aadfc684fe7599009a46107bd8
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-02-07 18:36:59 +0530

    now only take a  3 size of array,then after some after hrs i change this into n number of sized array

--- codeforce/A. Blackslex and Password.py
+++ codeforce/A. Blackslex and Password.py
@@ -1,22 +1,23 @@
-size=str(input())
-m=[]
-n=[]
-j=[]
-for i in range(len(size)):
-    print(i)
+size=int(input("enter a size of a array:"))
+for i in range(size):
     if i==0:
-        q=int(input())
-        m.append(q)
+        q=str(input("enter the 1st value:"))
+        num=0
+        for u in q:
+            num=num+int(u)
         continue
     elif i==1:
-        o=str(input())
-        n.append(o[0]+o[1])
-        print(n)
+        o=str(input("enter the 2nd value:"))
+        num1=0
+        for f in o:
+            num1=num1+int(f)
         continue
     else:
-        h=(input())
-        j.append(h)
+        h=str(input("enter the 3rd value:"))
+        num2=0
+        for l in h:
+            num2=num2+int(l)
         break
-m2=[]
-m2.append(m[0*1]+1)
-print(m2)
+print(num+1)
+print(num1+1)
+print(num2+1)
