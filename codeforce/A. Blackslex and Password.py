20521ca7a60fefa5f9e1aedba7c788e5f732d33b
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-02-07 19:01:05 +0530

    try to crt a code

--- codeforce/A. Blackslex and Password.py
+++ codeforce/A. Blackslex and Password.py
@@ -2,7 +2,7 @@ size=int(input("enter a size of a array:"))
 num=[]
 for i in range(1):
     for j in range(size):
-        q=str(input("enter the 1st value:"))
+        q=str(input(f"enter the {j+1}value:"))
         num.append(q)
 print(num)
 n=str(num)
