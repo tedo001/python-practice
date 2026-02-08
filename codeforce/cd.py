fe859afacb2f11713c3e018f4c69b6bec2471d6a
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-02-08 22:14:37 +0530

    code force

--- codeforce/cd.py
+++ codeforce/cd.py
@@ -1,15 +1,4 @@
-size = int(input())
-
-results = []
-
-for i in range(size):
-    value = input()
-    digit_sum = 0
-
-    for ch in value:
-        digit_sum += int(ch)
-
-    results.append(digit_sum + 1)
-
-for res in results:
-    print(res)
+t = int(input())
+for _ in range(t):
+    k, x = map(int, input().split())
+    print(k * x + 1)
