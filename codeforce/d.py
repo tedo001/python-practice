0f76defe21956399397383d6ed5658a2541937fc
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-02-09 23:58:03 +0530

    code force

--- codeforce/d.py
+++ codeforce/d.py
@@ -6,12 +6,3 @@ for _ in range(t):
         print("YES")
     else:
         print("NO")
-"""t = int(input())
-
-for _ in range(t):
-    a, b, c, d = map(int, input().split())
-    if a == b == c == d:
-        print("YES")
-    else:
-        print("NO")
-"""
