58733a971924925c32391a56264c84f75f9050b3
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-02-12 07:33:02 +0530

    code force

--- codeforce/cd.py
+++ codeforce/cd.py
@@ -1,4 +1,19 @@
-size=int(input())
-for i in range(size):
-    a,b=int((map(input().split())))
-    print(a*b+1)
+size=int(input("enter the size of array:"))
+arr1=[]
+arr2=[]
+arr3=[]
+i=0
+while i < size:
+    arr1_size=int(input(f"enter the size of array{size-1}:"))
+    for num1 in  range(arr1_size):
+        arr=int(input())
+        pivot=arr[0]
+        left=[]
+        right=[]
+        for num2 in
+        continue
+#IN SIZE ARRAY=2
+#INSIDE ARRAY 1=222222
+#OUTPUT IS 1
+#IN ARR 2=12354
+#OP IS 5
