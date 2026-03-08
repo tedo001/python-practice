de10a83d1506117cc8aaf975d655eebc07e2e3e0
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-03-08 12:20:11 +0530

    leanring part

--- inheritance/fruits.py
+++ inheritance/fruits.py
@@ -3,11 +3,9 @@ class fruits:
         self.name=name
         self.price=price
     def display(self):
-        print(self.name,self.price)
-f=fruits("apple",1234)
-
-
-n="apple"
-b=1234
-
+        print(self.name)
+        print(self.price)
+f=str(input("enter a fruit name:"))
+p=int(input(f"enter a price of a {f} fruit:"))
+f=fruits(f,p)
 f.display()
