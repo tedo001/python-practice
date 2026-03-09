6d4dce955e3e04264c58f3bf25a7812e53c4a824
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-03-09 13:19:26 +0530

    prefix max(code force)

--- codeforce/prefix max.py
+++ codeforce/prefix max.py
@@ -2,11 +2,14 @@ a=list(input())
 size=len(a)
 M=max(a)
 m=[]
-#count=0
+count=0
 for i in range(size):
       if a[i]==M:
           continue
       else:
           m.insert(i,a[i])
 m.insert(0,M)
-print(m)
+for l in range(size):
+    count=M+count
+print(count)
+
