dc57febb0b91928f26afc4a0152528127fa6fe70
durgamani <durgamani.d.e.c.e.50@gmail.com>
2026-05-14 15:56:21 +0530

    for user input

--- 2D slicing.py
+++ 2D slicing.py
@@ -1,9 +1,5 @@
 import numpy as np
-arr2 = np.array([
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-              ])
-print(arr2[1])#row slicing
-print(arr2[:, 1])# COLUM slicing
-print(arr2[0:2, 1:3])# extract subarray
+x=int(input("enter the x coordinate:"))
+y=int(input("enter the y coordinate:"))
+arr=np.array([x,y])
+print(arr)
