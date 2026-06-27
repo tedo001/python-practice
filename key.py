6b96161165ebe78c32bb73c1db9da93cf390c15b
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-06-27 11:36:52 +0530

    nn test code

--- key.py
+++ key.py
@@ -1,3 +1,8 @@
-import keyword
-print("The list of keywords are : ")
-print(keyword.kwlist)
+import numpy as np 
+x=np.array([[2,3,4],[4,5,6]])
+w=np.array([[2,4,5],[5,6,7]])
+b=0.1
+nn=(x*w)+b
+print(nn)
+act=max(0,nn)
+print("model nn is:",act)
