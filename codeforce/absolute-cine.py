8dd8939319b92e6cf7b29f9f666a40a35a331c31
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-07-16 07:32:42 +0530

    codeforce_code

--- codeforce/absolute-cine.py
+++ codeforce/absolute-cine.py
@@ -1,14 +1,14 @@
 
 lenth=int(input())
+C=[]
+D=[]
 for i in range(lenth):
     A=list(input())
     for k in range(len(A)):
-       C=[]
        J=int(input())
        C.append(J)
     B=list(input())
     for l in range(len(B)):
-       D=[]
        N=int(input())
        D.append(N)          
 
