2f37061fd022fd5bfefd90f1ec6d9f9bce0c5d88
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-07-16 07:28:13 +0530

    nothin

---
+++ codeforce/absolute-cine.py
@@ -0,0 +1,17 @@
+
+lenth=int(input())
+for i in range(lenth):
+    A=list(input())
+    for k in range(len(A)):
+       C=[]
+       J=int(input())
+       C.append(J)
+    B=list(input())
+    for l in range(len(B)):
+       D=[]
+       N=int(input())
+       D.append(N)          
+
+oput=max(C)+D
+print(oput)
+             
