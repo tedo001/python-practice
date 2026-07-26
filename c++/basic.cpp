92e51ee7827b92152ff4f34867ea0c23c9a46aa0
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-07-26 12:11:39 +0530

    c++ training

--- c++/basic.cpp
+++ c++/basic.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 int main(){
-     int a=1,b=2;
+     int a=23,b=2;
      int c=a;
      a=b;
      b=c;
-     std:: cout << "b value is :"b , "a value is :"a ;
+     std:: cout << a ;
      return 0;
 }
