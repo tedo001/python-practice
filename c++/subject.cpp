6fc723ce86e7d46181a924cae2a846a37b4c7d3b
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-07-27 20:37:25 +0530

    c++ training day2

---
+++ c++/subject.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+int main(){
+        std::string tittle = "subject check";
+	std::vector<string>={tamil,english,math,social,science};
+	int num[5] ={1,2,3,4,5};
+	int entrynum=0
+	// above three line is used to create a string ,vector and array variable 
+	std::cout << tittle << /n ;
+	std::cin << "enter the class like {1,2,3}" << entrynum;:L
+	std::cout << "into class " << entrynum << "standard";
+	for(i=0,i<entrynum,i++){
+
+	}
+	
+
+
+}
