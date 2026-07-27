b10866ad2ff2fc4b7f595ed700aee91ca9917bb8
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-07-27 20:23:23 +0530

    c++ training day 2

---
+++ c++/reverse.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main(){
+	int x,w = 0; 
+	std::string reverse ="";
+	std::cout << "enter the x value" << std::endl;
+	std::cin << "x:" << x ;
+	for(w=0,w<x,w++){
+	         reverse.pushback(x);
+		 }
+	std::cout << "reverse element : " << reverse ;
+	return 0;
+}
