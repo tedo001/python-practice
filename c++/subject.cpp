d878c23190d53363d14e55114027a3e6d63b9e7d
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-07-27 20:40:11 +0530

    c++ training day2

--- c++/subject.cpp
+++ c++/subject.cpp
@@ -7,9 +7,16 @@ int main(){
 	int entrynum=0
 	// above three line is used to create a string ,vector and array variable 
 	std::cout << tittle << /n ;
-	std::cin << "enter the class like {1,2,3}" << entrynum;:L
+	std::cin << "enter the class like {1,2,3}" << entrynum;
 	std::cout << "into class " << entrynum << "standard";
 	for(i=0,i<entrynum,i++){
+		if(entrynum==1){
+
+		}
+		elseif(entrynum==2){
+		}
+		else{
+		}
 
 	}
 	
