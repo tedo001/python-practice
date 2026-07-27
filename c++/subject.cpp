5cf66b690e82ec8a66523bf34acf8d523968488f
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-07-27 20:44:43 +0530

    c++ traing day2

--- c++/subject.cpp
+++ c++/subject.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 int main(){
         std::string tittle = "subject check";
-	std::vector<string>={tamil,english,math,social,science};
+	std::vector<string> subject={tamil,english,math,social,science};
 	int num[5] ={1,2,3,4,5};
 	int entrynum=0
 	// above three line is used to create a string ,vector and array variable 
@@ -11,12 +11,13 @@ int main(){
 	std::cout << "into class " << entrynum << "standard";
 	for(i=0,i<entrynum,i++){
 		if(entrynum==1){
+			std::cout<< "total subjects are " << subject.size()
 
 		}
-		elseif(entrynum==2){
-		}
 		else{
+			std::cout<< "skip";
 		}
+		
 
 	}
 	
