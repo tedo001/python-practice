5179191f62d8530d463c2102d8686d91cd2765f2
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-07-31 13:38:30 +0530

    largest number

---
+++ c++/largestarr.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cmath>
+int main(){
+       int num[]={};
+       std::cout << "enter the array size:";
+       std::cin >> num;
+       for(int nums : num){
+	       emp_num=nums
+           for(int i=0,i<sizeof(num),i++){
+		  if(emp_num > num[i]){
+			  std::cout << "elment" << emp_num << "is a largest num"; }
+		  else if(emp_num==num[i]){
+			  continue;
+		  }
+		  else if(){
+		  }
+		  else{
+		  }
+	     }}
+        return 0;
+}
