bb69d15ea377c6abd8b1ca8c5f00dba9ec8d65bb
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-08-01 07:44:31 +0530

    factorial in c++

--- c++/factorial.cpp
+++ c++/factorial.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 int main()
 {
-    int total = 1;
-    int n,a ;
+    int total = 1; //assing the one value
+    int n,a ; //for input terms 
     cout << "enter number for factorial :" ;
     cin >> n ;cout<< "\n";
-    a = n;
+    a = n; // for safe swap
 top:
     if (a != 0)
     {
@@ -21,3 +21,6 @@ top:
 
     return 0;
 }
+/*this one of a recursion using a top ... goto top aproch 
+ *in this methode is a one of a easy and finest methode 
+ */
