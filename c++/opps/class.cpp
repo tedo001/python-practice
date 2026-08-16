2088c0b86cd7df8aa8947f583ef41b80534a1553
tedo001 <durgamani.d.e.c.e.50@gmail.com>
2026-08-16 11:43:13 +0530

    opps in c++

---
+++ c++/opps/class.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class mani {
+	public:
+		int rollno;
+		int phno;
+		string name;
+};
+
+
+int main(){
+     mani raj;
+     raj.rollno=3234;
+     raj.name="gopal";
+     cout<< raj.name <<'\n';	     
+return 0;
+}
+
+
